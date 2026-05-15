bbab2c5f4a4b31928481b191cd9671c87093b6dd
premdeveloper9894 <248840338+premdeveloper9894@users.noreply.github.com>
2026-05-15 09:09:13 +0530

    Durga sir
    alterate using durga sir videos

--- hellow_world/src/oops/Sysnchronized.java
+++ hellow_world/src/oops/Sysnchronized.java
@@ -7,12 +7,21 @@ public class Sysnchronized {
 	public static void main(String[] args) {
 		
 		/*
-		   > Synchronized is a keyword in Java used 
-		     to control access to a block of code or method so that only one thread can run it at a time.
-		     
-		   > We can overcome data inconsidency problem.
+		   > Synchronized is a modifier in Java used We can overcome data inconsidency problem.
+		     if multiple threads are try to operatate simunteneouly on the same object, then there may be chance of 
+			 data inconsisdenc prohblem. to overcome this problem we should go for synchronized keyword. if the method or block declare as a
+		     synchronized then at a time only on thread allow to execute that method or block on the given object. So that data 
+			 inconsisdency problem will be resolved.
 		   
 		   > Applicable only Method and block should not for use class and variable.
+
+		    Advantage.
+			
+           > The biggest advantage of synchronized keyword is We can resolve data inconsisdency problem.
+		   
+		   > Disadvantage
+		    The disadvagage of the synchronized keywords is  
+            it increases waiting time of the thread and creat performance problem.
 		   
 		   eg: 
 		     > telephone - oruthal pesiya piragu than innoruthar pesa mudiyum:
